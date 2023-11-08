7cab6b6d91101fe57ed3f183c5d2dd9291fee481
Alec Tutin <a.tutin@qut.edu.au>
2023-11-08 17:34:48 +1000

    Ported the example package to ROS2

--- ros_upgrade_example/upgrade_example.py
+++ ros_upgrade_example/upgrade_example.py
@@ -1,48 +1,49 @@
 #!/usr/bin/env python
 
-import rospy
-from std_msgs.msg import Bool, String, Time
+import rclpy
+from rclpy.node import Node, Publisher, Parameter
+from rclpy.qos import QoSProfile, DurabilityPolicy, ReliabilityPolicy
+from std_msgs.msg import Bool, String
+from builtin_interfaces.msg import Time
 from ros_upgrade_example.msg import ExampleMsg
 
 node_name: str = "talker_node"
 version: str = "0.1"
 
-class TalkerNode:
+class TalkerNode(Node):
     def __init__(self) -> None:
-        rospy.init_node(node_name)
-        rospy.loginfo(f"[{rospy.get_caller_id()}] Starting {node_name} v{version}...")
+        super().__init__(node_name)
+        self.get_logger().info(f"Starting {node_name} v{version}...")
 
-        self.publisher_chatter: rospy.Publisher = rospy.Publisher("~chatter", String, queue_size=10)
+        self.declare_parameter("message_prefix", "The time is")
 
-        publisher_latched: rospy.Publisher = rospy.Publisher("~startup_time", Time, queue_size=1, latch=True)
-        publisher_latched.publish(rospy.get_rostime())
+        self.publisher_chatter: Publisher = self.create_publisher(String, "~/chatter", qos_profile=10)
+
+        qos_profile_latched: QoSProfile = QoSProfile(durability=DurabilityPolicy.TRANSIENT_LOCAL, reliability=ReliabilityPolicy.RELIABLE, depth=1)
+        publisher_latched: Publisher = self.create_publisher(Time, "~/startup_time", qos_profile=qos_profile_latched)
+        publisher_latched.publish(self.get_clock().now().to_msg())
 
         self.should_publish: bool = True
-        rospy.Subscriber("~should_publish", Bool, self.callback_should_publish)
+        self.create_subscription(Bool, "~/should_publish", self.callback_should_publish, 1)
 
-        rospy.Subscriber("~new_msg", ExampleMsg, self.callback_new_msg)
-        
-        self.rate: rospy.Rate = rospy.Rate(10)
+        self.create_subscription(ExampleMsg, "~/new_msg", self.callback_new_msg, 1)
 
-    def spin(self) -> None:
-        rospy.loginfo(f"[{rospy.get_caller_id()}] Spinning...")
-        while not rospy.is_shutdown():
-            if self.should_publish:
-                message_prefix: str = rospy.get_param("~message_prefix", "The time is")
-                self.publisher_chatter.publish(f"{message_prefix}: {rospy.get_time()}")
-            
-            self.rate.sleep()
+        self.create_timer(0.1, self.on_timer)
+
+    def on_timer(self) -> None:
+        if self.should_publish:
+            parameter_prefix: Parameter = self.get_parameter("message_prefix")
+            self.publisher_chatter.publish(String(data=f"{parameter_prefix.get_parameter_value().string_value}: {self.get_clock().now()}"))
 
     def callback_should_publish(self, message: Bool) -> None:
         self.should_publish = message.data
     
     def callback_new_msg(self, message: ExampleMsg) -> None:
-        rospy.loginfo(f"[{rospy.get_caller_id()}] Received a very important number: {message.important_number}")
+        self.get_logger().info(f"Received a very important number: {message.important_number}")
 
 
 if __name__ == "__main__":
+    rclpy.init()
     node: TalkerNode = TalkerNode()
-    try:
-        node.spin()
-    except rospy.ROSInterruptException:
-        pass
+    node.get_logger().info("Spinning...")
+    rclpy.spin(node)
